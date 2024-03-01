6275036aa874c90fc98b29b2e62f287df9cf106e
aayushmanocha <aayush.manocha@gmail.com>
2024-03-01 12:21:35 -0500

    Refactor create new message form to its own component

--- frontend/app/routes/chats.$chatid.tsx
+++ frontend/app/routes/chats.$chatid.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "@remix-run/react"
 import { set } from "node_modules/cypress/types/lodash"
 import { useContext, useMemo, useState } from "react"
+import NewMessageForm from "~/components/NewMessageForm"
 import useAuthenticationGuard from "~/hooks/useAuthenticationGuard"
 import useCreateMessageMutation from "~/hooks/useCreateMessageMutation"
 import { useGetChatByIdService } from "~/hooks/useGetChatByIdService"
@@ -14,26 +15,8 @@ export default function ChatDetailPage() {
   const { user } = useContext(UserContext)
 
 
-  const createMessageMutation = useCreateMessageMutation(chatid)
   const { isLoading, isError, data: chatMessagesData } = useGetChatMessagesService(chatid)
   const { isLoading: chatIsLoading, isError: chatIsError, data: chatData } = useGetChatByIdService(chatid)
-
-  const to_id = useMemo(() => {
-    const user1_id = chatData?.data?.user1_id
-    const user2_id = chatData?.data?.user2_id
-
-    return user?.id == user1_id ? user2_id : user1_id
-  }, [chatData, user])
-
-
-  const [message, setMessage] = useState('')
-
-  const handleSubmitMessage = (e: any) => {
-    e.preventDefault()
-    const to = to_id.toString()
-    createMessageMutation({ message, to_id: to })
-    setMessage('')
-  }
 
   if (isLoading) return <div>Loading...</div>
   if (isError) return <div>Error</div>
@@ -48,10 +31,7 @@ export default function ChatDetailPage() {
           )
         })}
       </ul>
-      <form onSubmit={handleSubmitMessage}>
-        <input value={message} onChange={e => setMessage(e.target.value)} type="text" placeholder="Please enter a message..." />
-        <button type="submit">Send</button>
-      </form>
+      <NewMessageForm chatData={chatData} />
     </div>
   )
 }
